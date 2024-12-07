9be06ea73b42d867dc8acbfa0906e782c07e3ace
Sergey Ogurtsov <sergey.ogurtsov@gmail.com>
2024-12-07 16:47:43 +0300

    DZ#5

---
+++ app/src/main/java/ru/gb/dz5/MainActivity.kt
@@ -0,0 +1,17 @@
+package ru.gb.dz5
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import ru.gb.dz5.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
